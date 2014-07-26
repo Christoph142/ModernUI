1e58aa5eb9c7803c075ebf368b711e06bd68df73
Christoph142 <Christoph142@gmx.com>
2014-07-26 16:02:36 +0200

    remove old junk

--- extension/background.js
+++ extension/background.js
@@ -12,23 +12,3 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse){
 	else if (request.data === "isFullscreen") 	sendResponse({ data : "isFullscreen", isFullscreen : (currentWindow.state === "fullscreen" ? true : false) });
 	else if (request.data === "activateTab") 	chrome.tabs.update(request.id, {active: true});
 });
-
-/*chrome.windows.getAll(
-{
-	for(var i in windows)
-	{
-	  for(var j in windows[i].tabs)
-	  {
-		original.push(new tabInfo(windows[i].tabs[j], j));
-		alert(original[i*j].tab.title);
-		original[i*j].tab.title = tab.title;
-		original[i*j].tab.url = tab.url;
-		original[i*j].tab.status = tab.status;
-		original[i*j].count = 0;
-
-		localstorage.setitem["tab.title"];
-		localstorage.setitem["tab.url"];
-		localstorage.setitem["tab.status"];
-	   }
-	}
-});*/
